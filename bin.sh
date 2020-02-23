817049b91dba49c9fd93be2e25548180912a0850
Tony Xiong <tonyxiong.xss@aliyun.com>
2020-02-23 23:07:16 +0800

    change floder

--- bin.sh
+++ bin.sh
@@ -2,11 +2,10 @@
 # go env docker
 docker run -d --name goenv \
     -v /d/project:/data \
-    --link kafka:kafka
     --link rabbitmq:rabbitmq
     -p 8888:8888 \
     -e LISTEN_PORT=8888 \
-    -e STORAGE_ROOT=/data/tmpStore \
+    -e STORAGE_ROOT=/home/bigsomg/project/go-storage/tmpStore/tmp \
     -it golang bash
 
 # run
