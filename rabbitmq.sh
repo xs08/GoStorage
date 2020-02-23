6aebc7dd406c858f7713858c85f1df42720e3228
Tony Xiong <tonyxiong.xss@aliyun.com>
2020-02-23 10:37:41 +0800

    add exchange to rabbitmq

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -7,3 +7,17 @@ docker run -d --name rabbitmq \
     -e RABBITMQ_DEFAULT_USER=admin \
     -e RABBITMQ_DEFAULT_PASS=admin \
     -it rabbitmq:management
+
+# 使用RabbitMQAdmin管理工具创建exchange
+# 1. 远程服务安装开启rabbitmqadmin
+# sudo apt-get install rabbitmq-server
+# 2. 启用管理插件
+# sudo rabbitmq-plugins enable rabbitmq_managment
+# 3. 下载管理插件到本地
+# wget lcoalhost:15672/cli/rabbitmqadmin
+# 4. 使用python3执行命令
+# python3 rabbitmqadmin declare exchange name=exchangeName type=exchangeType
+
+# our service needs following exchange
+rabbitmqadmin declare exchange name=apiServers type=fanout
+rabbitmqadmin declare exchange name=dataServers type=fanout
